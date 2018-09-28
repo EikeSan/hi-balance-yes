56f5d0b25000a7157b0de7571aeb4d5ee86573d5
EikeSan <eikesantz@hotmail.com>
2018-09-28 11:48:32 -0300

    feat(Swagger): Add redirection on home path to swagger url

--- src/main/java/com/hiBalanceYes/controller/HomeController.java
+++ src/main/java/com/hiBalanceYes/controller/HomeController.java
@@ -2,13 +2,14 @@ package com.hiBalanceYes.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class HomeController {
 
     @GetMapping(value = "/")
-    public String home(){
-        return "Hello";
+    public RedirectView home(){
+        return new RedirectView("swagger-ui.html");
     }
 
     @GetMapping(value = "/private")
