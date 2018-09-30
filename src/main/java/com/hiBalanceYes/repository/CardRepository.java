15e15abd9a8dbaf53c11d14309ab3e82a2c58e3d
EikeSan <eikesantz@hotmail.com>
2018-09-30 00:12:26 -0300

    fix(Card): Add import card model

--- src/main/java/com/hiBalanceYes/repository/CardRepository.java
+++ src/main/java/com/hiBalanceYes/repository/CardRepository.java
@@ -1,5 +1,7 @@
 package com.hiBalanceYes.repository;
 
+import com.hiBalanceYes.model.Card;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface  CardRepository extends JpaRepository<Card, Long> {
